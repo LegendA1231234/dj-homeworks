c9118f8c414ddc7e4560ad58e547dbac6cfc0a65
LegendA1231234 <sasha.ivonin228228@gmail.com>
2025-12-16 22:00:14 +0300

    Update views.py

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,15 +1,30 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.exceptions import PermissionDenied
+from django_filters.rest_framework import DjangoFilterBackend
+
+from advertisements.models import Advertisement
+from advertisements.serializers import AdvertisementSerializer
+from advertisements.filters import AdvertisementFilter
 
 
 class AdvertisementViewSet(ModelViewSet):
-    """ViewSet для объявлений."""
-
-    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
-    #   сериализаторов и фильтров
+    queryset = Advertisement.objects.all().order_by('-created_at')
+    serializer_class = AdvertisementSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = AdvertisementFilter
 
     def get_permissions(self):
-        """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
+        if self.action in ("create", "update", "partial_update", "destroy"):
             return [IsAuthenticated()]
         return []
+
+    def perform_destroy(self, instance):
+        if instance.creator != self.request.user:
+            raise PermissionDenied("Удалять можно только свои объявления")
+        instance.delete()
+
+    def perform_update(self, serializer):
+        if serializer.instance.creator != self.request.user:
+            raise PermissionDenied("Редактировать можно только свои объявления")
+        serializer.save()
